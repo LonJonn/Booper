6619bd22ca39b79072332a3e1ae8dc5f2cd82326
LonJonn <leon.salsiccia@gmail.com>
2020-06-28 00:46:46 +1000

    Cleaned search.js

---
+++ src/search.ts
@@ -0,0 +1,63 @@
+import Fuse from "fuse.js";
+import {
+	nodeIsVisible,
+	cleanNodeText,
+	removeRelatedButtons,
+	CleanedElement,
+} from "./utils";
+
+/**
+ * Will run every mount.
+ *
+ * Finds all the currently visible button/boop nodes
+ */
+export function getVisibleNodes() {
+	let nodes = [...document.querySelectorAll("[role='button']")];
+
+	// prettier-ignore
+	return nodes
+		.filter(nodeIsVisible)
+		.map(cleanNodeText)
+		.reduce(removeRelatedButtons, []);
+}
+
+/**
+ * Fuzzy search the text content of Elements
+ *
+ * @param nodes HTML Elements to search through
+ * @param query Query to search by
+ */
+export function search(nodes: CleanedElement[], query: string) {
+	const fuse = new Fuse(nodes, {
+		keys: ["cleanText"],
+		includeScore: true,
+		includeMatches: true,
+	});
+
+	return fuse.search(query);
+}
+
+/**
+ * Highlight HTML Elements
+ *
+ * @param results HTML Element search results
+ */
+export function applyStyles(results: Fuse.FuseResult<CleanedElement>[]) {
+	for (const result of results) {
+		const node = result.item;
+		node.style.outline = "solid 3px yellow";
+	}
+}
+
+/**
+ * Undo style changes performed on HTML Elements
+ * by the `applyStyles` function.
+ *
+ * @param results HTML Element search results
+ */
+export function clearStyles(results: Fuse.FuseResult<CleanedElement>[]) {
+	for (const result of results) {
+		const node = result.item;
+		node.style.outline = "";
+	}
+}
--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@
  * A modified interface that extends the generic `HTMLElement`
  * interface to include the `cleanText` property
  */
-interface CleanedElement extends HTMLElement {
+export interface CleanedElement extends HTMLElement {
 	cleanText?: string;
 }
 
@@ -11,7 +11,7 @@ interface CleanedElement extends HTMLElement {
  *
  * @param node HTML Element to check
  */
-function nodeIsVisible(node: HTMLElement) {
+export function nodeIsVisible(node: HTMLElement) {
 	// prettier-ignore
 	let nodeNotVisible =
 		!node.offsetParent ||
@@ -22,25 +22,28 @@ function nodeIsVisible(node: HTMLElement) {
 }
 
 /**
- * Will santize the text content of a HTML element and
+ * Santise the text content of a HTML element and
  * store it's value onto a new `cleanText` property.
  *
  * @param node HTML Element to clean
  * @returns The cleaned HTML Element
  */
-function cleanNodeText(node: CleanedElement) {
+export function cleanNodeText(node: CleanedElement) {
 	node.cleanText = node.textContent.trim().toLowerCase();
 
 	return node;
 }
 
 /**
- * A Reducer function to remove nested buttons/boops
+ * A reducer function to remove nested buttons/boops
  *
  * @param acc HTML Element Accumulator
  * @param currentNode Current HTML Element
  */
-function removeRelatedButtons(acc: HTMLElement[], currentNode: HTMLElement) {
+export function removeRelatedButtons(
+	acc: HTMLElement[],
+	currentNode: HTMLElement
+) {
 	const hasRelative = acc.some((node) => {
 		const isChild = node.contains(currentNode);
 		const isParent = currentNode.contains(node);
@@ -58,7 +61,7 @@ function removeRelatedButtons(acc: HTMLElement[], currentNode: HTMLElement) {
  *
  * @param node HTML Element to check
  */
-function isInViewport(node: HTMLElement) {
+export function isInViewport(node: HTMLElement) {
 	const boundingRect = node.getBoundingClientRect();
 	const height = window.innerHeight || document.documentElement.clientHeight;
 	const width = window.innerWidth || document.documentElement.clientWidth;
